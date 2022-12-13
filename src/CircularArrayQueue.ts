0cbaa0249ad8f83cd62c84febe99008d000b1578
engineer <noemail>
2022-12-13 01:33:49 +0000

    ENH: document code

--- src/CircularArrayQueue.ts
+++ src/CircularArrayQueue.ts
@@ -1,26 +1,76 @@
 import IQueue from "./Queue";
 
+/**
+ * Circular array queue -- an implementation of the Queue ADT using a circular
+ * array along with a dynamic resizing scheme.
+ *
+ * @class CircularArrayQueue
+ * @typedef {CircularArrayQueue}
+ * @template T The type of all elements in the queue.
+ * @implements {IQueue<T>}
+ */
 class CircularArrayQueue<T> implements IQueue<T> {
+    /**
+     * The underlying array for storing queue elements.
+     * @type {T[]} 
+     */
     #elems: T[];
+
+    /**
+     * Position of the front element in the underlying array.
+     *
+     * @type {number}
+     */
     #startIdx: number;
+
+    /**
+     * Number of elements in the queue.
+     * @type {number}
+     */
     #numElems: number;
 
+    /**
+     * Create an empty queue.
+     * @constructor
+     * @param {number} [initCapacity=2] Initial anticipated number of elements
+     *      to be stored in the queue.
+     */
     constructor(initCapacity: number = 2) {
         this.#elems = new Array(initCapacity);
         this.#startIdx = 0;
         this.#numElems = 0;
     }
 
+    /**
+     * The maximum number of elements the queue can store without allocating
+     * additional memory.
+     * @readonly
+     * @type {number}
+     */
     get #capacity(): number {
         return this.#elems.length;
     }
 
+    /**
+     * Position of the one past the last queue element in the underlying array.
+     * @readonly
+     * @type {number}
+     */
     get #endIdx(): number {
         return (this.#startIdx + this.#numElems) / this.#capacity
     }
 
+    /**
+     * Growth factor for the underlying array.
+     * @static
+     * @type {number}
+     */
     static #growthFactor: number = 2;
 
+    /**
+     * Grow or shrink the underlying array by a factor of 2.
+     * @param {boolean} [grow=true]
+     */
     #resize(grow: boolean = true): void {
         if (grow) {
             if (this.#numElems === this.#capacity) {
@@ -37,14 +87,29 @@ class CircularArrayQueue<T> implements IQueue<T> {
         }
     }
 
+    /**
+     * Number of elements in the queue.
+     * @readonly
+     * @type {number}
+     */
     get size(): number {
         return this.#numElems;    
     }
 
+    /**
+     * Whether the queue has no elements.
+     * @returns {boolean} True if empty.
+     */
     empty(): boolean {
         return this.#numElems === 0;
     }
 
+    /**
+     * Iterate over all elements in the queue from the front to generate 
+     * a string representation of the queue.
+     * @param {string} separator - Element separator.
+     * @returns {(string | null)} The elements in front-to-end order.
+     */
     iter(separator: string = ' '): string | null {
         if (this.size === 0) {
             return null;
@@ -56,6 +121,10 @@ class CircularArrayQueue<T> implements IQueue<T> {
         return frontToEnd;
     }
 
+    /**
+     * Get the element at the front of the queue if not empty.
+     * @returns {(T | null)} The front element.
+     */
     front(): T | null {
         if (this.empty()) {
             return null;
@@ -63,12 +132,20 @@ class CircularArrayQueue<T> implements IQueue<T> {
         return this.#elems[this.#startIdx];
     }
 
+    /**
+     * Add an element to the end of the queue.
+     * @param {T} elem - The elemen to be added.
+     */
     enqueue(elem: T): void {
         this.#resize();
         this.#elems[this.#startIdx] = elem;
         ++this.#numElems;
     }
 
+    /**
+     * Remove the front element from the queue if not empty.
+     * @returns {boolean} True if success.
+     */
     deque(): boolean {
         if (this.empty()) {
             return false;
--- src/Queue.ts
+++ src/Queue.ts
@@ -1,9 +1,47 @@
+/**
+ * The Queue ADT
+ * @interface IQueue
+ * @typedef {IQueue}
+ * @template T The type of all elements in the queue.
+ */
 interface IQueue<T> {
+    /**
+     * Number of elements in the queue.
+     * @readonly
+     * @type {number}
+     */
     get size(): number;
+
+    /**
+     * Whether the queue has no elements.
+     * @returns {boolean} True if empty.
+     */
     empty(): boolean;
+
+    /**
+     * Iterate over all elements in the queue from the front to generate 
+     * a string representation of the queue.
+     * @param {string} separator - Element separator.
+     * @returns {(string | null)} The elements in front-to-end order.
+     */
     iter(separator: string): string | null;
+
+    /**
+     * Get the element at the front of the queue if not empty.
+     * @returns {(T | null)} The front element.
+     */
     front(): T | null;
+
+    /**
+     * Add an element to the end of the queue.
+     * @param {T} elem - The elemen to be added.
+     */
     enqueue(elem: T): void;
+
+    /**
+     * Remove the front element from the queue if not empty.
+     * @returns {boolean} True if success.
+     */
     deque(): boolean;
 }
 
