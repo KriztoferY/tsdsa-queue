db0e18e5a15be86c52a231877031e42ddabea711
engineer <noemail>
2023-01-09 17:04:22 +0000

    add new function to clione a queue

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,7 @@
 import IQueue from "./Queue";
 import CircArrayQueue from "./CircArrayQueue";
+import SLListQueue from './SLListQueue';
+
 
 /**
  * Tests if a queue is of the circular array queue type.
@@ -13,4 +15,23 @@ function isCircArrayQueue<T>(queue: IQueue<T>): queue is CircArrayQueue<T> {
     return queue instanceof CircArrayQueue<T>;
 }
 
-export { isCircArrayQueue };
+
+/**
+ * Creates a deep copy of a queue.
+ * 
+ * @template T Type of the elements of the queue to clone.
+ * @param queue The queue to clone.
+ * @returns The clone.
+ */
+function cloneQueue<T>(queue: IQueue<T>): IQueue<T> {
+    const clone = isCircArrayQueue(queue) 
+        ? new CircArrayQueue<T>(queue.capacity)
+        : new SLListQueue<T>();
+
+    queue.iter((elem: T) => clone.enqueue(elem));
+
+    return clone;
+}
+
+
+export { isCircArrayQueue, cloneQueue };
